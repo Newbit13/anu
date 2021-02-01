afb2cc00e347e1943eb3983b6de666bddbadf4e9
Newbit13 <525842854@qq.com>
2021-02-01 12:26:07 +0800

    debug

---
+++ start.js
@@ -0,0 +1,39 @@
+import anu from './src/index';
+var h = anu.createElement
+class Hello extends anu.Component {
+    constructor() {
+        super() //Must write, or throw ReferenceError: |this| used uninitialized in Hello class constructor
+        this.handleClick = this.handleClick.bind(this)
+    }
+    componentWillMount() {
+        console.log('准备插入DOM树')
+    }
+    handleClick() {
+        this.setState({
+            title: new Date - 0,
+            child: new Date - 1
+        })
+    }
+    static className() {
+        return 'Point';
+    }
+    render() {
+        return h('div', {
+            title: this.state.title,
+            onClick: this.handleClick
+        }, this.state.child || "x")
+    }
+}
+
+function main() {
+    var vnode = h('h2', null, 'A', '你好', h(Hello), h('p'));
+    return vnode;
+}
+window.onload = function () {
+    // var vnode1 = main();
+    // var vnode2 = main();
+    // console.log(vnode1.props === vnode2.props);
+    //render4个参数， vnode, container, callback, clearContainer
+    var result = anu.render(main(), document.body, null, false)
+    console.log(result)
+}
