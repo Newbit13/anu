6326cd1badd92ac4618ad31004938188bc7b764d
Newbit13 <525842854@qq.com>
2021-02-01 17:31:28 +0800

    bug:key 相同时不会比较子元素了

--- start.js
+++ start.js
@@ -1,27 +1,56 @@
 import anu from './src/index';
 var h = anu.createElement
+
 class Hello extends anu.Component {
     constructor() {
         super() //Must write, or throw ReferenceError: |this| used uninitialized in Hello class constructor
         this.handleClick = this.handleClick.bind(this)
+        this.state = {
+            list: [{
+                key: 1,
+                content: '111'
+            }, {
+                key: 2,
+                content: '222'
+            }]
+        }
     }
     componentWillMount() {
         console.log('准备插入DOM树')
     }
     handleClick() {
+        // this.setState({
+        //     title: new Date - 0,
+        //     child: new Date - 1
+        // })
         this.setState({
-            title: new Date - 0,
-            child: new Date - 1
+            list: [{
+                key: 2,
+                content: '333'
+            }, {
+                key: 1,
+                content: '444'
+            }]
         })
     }
     static className() {
         return 'Point';
     }
     render() {
+        // return h('div', {
+        //     title: this.state.title,
+        //     onClick: this.handleClick
+        // }, this.state.child || "x")
         return h('div', {
             title: this.state.title,
             onClick: this.handleClick
-        }, this.state.child || "x")
+        }, this.state.list.map(item => {
+            return h(
+                'div',
+                { key: item.key },
+                [h('div', null, item.content)]
+            )
+        }))
     }
 }
 
@@ -34,6 +63,6 @@ window.onload = function () {
     // var vnode2 = main();
     // console.log(vnode1.props === vnode2.props);
     //render4个参数， vnode, container, callback, clearContainer
-    var result = anu.render(main(), document.body, null, false)
-    console.log(result)
+    var result = anu.render(main(), document.getElementById('app'), null, false)
+    console.log(result);
 }
